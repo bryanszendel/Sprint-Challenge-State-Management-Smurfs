5624209124241bbfbdfd238fd050037fd09213a5
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-16 10:09:56 -0600

    GET request to smurf api is returning smurf data

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,12 +1,35 @@
 
+import { 
+  GET_SMURFS,
+  GET_SMURFS_SUCCESS,
+  // GET_SMURFS_FAILURE
+ } from '../actions'
+
 const initialState = {
-  smurfs: []
+  smurfs: [],
+  isLoading: false,
+  error: ''
 };
 
 export const smurfReducer = (state = initialState, action) => {
   switch (action.type) {
-    
-    
+    case GET_SMURFS:
+      return {
+        ...state,
+        isLoading: true,
+        error: ''
+      }
+    case GET_SMURFS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        smurfs: action.payload,
+        error: ''
+      }
+    // case GET_SMURFS_FAILURE:
+    //   return {
+
+    //   }
     default:
       return state;
   }
