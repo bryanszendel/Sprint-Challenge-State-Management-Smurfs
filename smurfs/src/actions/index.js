293d55288acf29f1f9ac3d017f0b53b5d702db5a
Bryan Szendel <bryan.szendel@gmail.com>
2019-08-16 12:29:37 -0600

    MVP COMPLETE - POST request is creating new smurfs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -10,6 +10,7 @@ export const getData = () => {
     axios
       .get('http://localhost:3333/smurfs')
       .then(res => {
+        console.log(res.data)
         dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data })
       })
       .catch(err => {
@@ -18,3 +19,19 @@ export const getData = () => {
       })
   }
 }
+
+export const ADD_SMURF = 'ADD_SMURF';
+
+export const addSmurf = smurf => dispatch => {
+    return axios
+      .post('http://localhost:3333/smurfs', smurf)
+      .then(res => {
+        console.log(res)
+        const smurfAdded = { type: ADD_SMURF, payload: res.data.smurf }
+        dispatch(smurfAdded)
+      })
+      .catch(err => {
+        console.log(err.response)
+      })
+  }
+  
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,7 @@
 import { 
   GET_SMURFS,
   GET_SMURFS_SUCCESS,
+  ADD_SMURF,
   // GET_SMURFS_FAILURE
  } from '../actions'
 
@@ -28,9 +29,34 @@ export const smurfReducer = (state = initialState, action) => {
       }
     // case GET_SMURFS_FAILURE:
     //   return {
-
+    
     //   }
+    case ADD_SMURF:
+      return {
+        ...state,
+        smurfs: [...state.smurfs, action.payload]
+      }
     default:
       return state;
   }
 }
+
+// export const addSmurfState = {
+//   name: '',
+//   age: null,
+//   height: ''
+// }
+
+// export const addSmurfReducer = (state, action) => {
+//   switch (action.type) {
+//     case 'ADDING_SMURF_ON_FORM':
+//       return {
+//         ...state,
+//         name: action.payload,
+//         age: action.payload,
+//         height: action.payload
+//       }
+//     default:
+//       return state;
+//   }
+// }
